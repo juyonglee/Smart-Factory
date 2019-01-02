6c7c52741112b82e9afd8e51e305b4bfa878840e
juyonglee <juyonglee0208@gmail.com>
2019-01-02 22:58:22 +0900

    Factory Device 역활을 하는 Node 구현

---
+++ DeviceNode.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+const client = net.connect({ port: 8124, host: '127.0.0.1', family: 4 });
+var deviceSendData;
+
+client.on('connect', function () {
+    console.log('Client connect!');
+    if (!process.argv[2]) {
+        deviceSendData = "Device TEST"
+    } else {
+        deviceSendData = process.argv[2]
+    }
+});
+
+client.on('ready', function () {
+    console.log('Client ready!');
+    setInterval(function () {
+        client.write(deviceSendData);
+    }, 1000);
+});
+
+client.on('end', () => {
+    console.log('Disconnected from Device Server!');
+});
