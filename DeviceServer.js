8e6d2c720a501bea73c39a6da57eecc0b49b1b4b
juyonglee <juyonglee0208@gmail.com>
2019-01-02 23:55:02 +0900

    Socket.IO Client 및 ID 등록 기능 추가

--- DeviceServer.js
+++ DeviceServer.js
@@ -1,10 +1,17 @@
 const net = require('net');
+const io = require('socket.io-client');
 
 const server = net.createServer((client) => {
-    // 'Connection' listener
-    client.on('data', function(data){
-      console.log(data.toString());
-    });
-  }).listen(8124, '127.0.0.1', () => {
-    console.log('Device Server Listening...');
+  // 'Connection' listener
+  var chatting_socket = io("http://localhost:4000");
+  
+  chatting_socket.on('id_registration', function(id){
+    console.log("Device Chatting Socket ID:", chatting_socket.id);
   });
+  
+  client.on('data', function (data) {
+    console.log(data.toString());
+  });
+}).listen(8124, '127.0.0.1', () => {
+  console.log('Device Server Listening...');
+});
